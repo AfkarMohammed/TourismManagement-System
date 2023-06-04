9776234a19840156c7a3ef033926923df2b51929
Nirmala <nirmalawickramasinghe16@gmail.com>
2023-06-04 19:48:22 +0530

    2st Commit

--- src/main/java/tours/com/TourismManagement/System/controller/FeedbackController.java
+++ src/main/java/tours/com/TourismManagement/System/controller/FeedbackController.java
@@ -1,5 +1,48 @@
 package tours.com.TourismManagement.System.controller;
 
-public class FeedbackController {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
-}
+@Controller
+public class FeedbackController {
+    @Autowired
+    private FeedbackService feedbackService;
+
+    @GetMapping("/read-feedback")
+    public String showreadFeedbackPage(Model model){
+        model.addAttribute("feedbacks", feedbackService.findAll());
+        return "readfeedback";
+    }
+    @GetMapping("/create-feedback")
+    public String showCreateFeedbackPage(Model model){
+        model.addAttribute("command", new Feedback());
+        return "createfeedback";
+    }
+    @PostMapping("/create-feedback")
+    public String createFeedback(@ModelAttribute("feedback")Feedback feedback){
+        feedbackService.saveFeedback(feedback);
+        return "redirect:/read-feedback";
+    }
+
+    @GetMapping("/update-feedback/{id}")
+    public String showupdateFeedbackPage(@PathVariable int id, Model model){
+        model.addAttribute("id", id);
+        model.addAttribute("command", feedbackService.findById(id).orElse(null));
+        return "updatefeedback";
+    }
+    @PostMapping("/update-feedback/{id}")
+    public String updateFeedback(@PathVariable int id, @ModelAttribute("feedback") Feedback feedback){
+        feedbackService.updatefeedback(id, feedback);
+        return "redirect:/read-feedback";
+    }
+
+    @GetMapping("/delete-feedback/{id}")
+    public String deleteFeedback(@PathVariable int id){
+        feedbackService.deleteById(id);
+        return "redirect:/read-feedback";
+    }
