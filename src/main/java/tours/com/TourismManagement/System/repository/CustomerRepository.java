c1ece2bbb920abd0a5ede25348f01715eddc5738
Rizky <mohammedrizky50@gmail.com>
2023-06-10 13:21:16 +0530

    final commit

---
+++ src/main/java/tours/com/TourismManagement/System/repository/CustomerRepository.java
@@ -0,0 +1,12 @@
+package  tours.com.TourismManagement.System.repository;
+
+import  tours.com.TourismManagement.System.model.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface CustomerRepository extends JpaRepository <Customer, Integer> {
+    Optional<Customer> findByEmailAndPassword(String email, String password);
+}
